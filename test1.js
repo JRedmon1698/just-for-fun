c94c65152b53a695f48d5117da7ae6d5184a5481
Joe Redmon <joesredmon@gmail.com>
2020-04-21 11:04:35 -0700

    added js file to parent repo

---
+++ test1.js
@@ -0,0 +1,11 @@
+// // ellipse (100, 100, 100, 100);
+
+// function greeting(){
+//     var hello = prompt("Hello!");
+// return document.write("Hello");
+// };
+
+function askName() {
+    var userName = prompt('What is your name?');
+    return document.write('<h3>' + 'Hello, ' + userName + '</h3>');
+};
