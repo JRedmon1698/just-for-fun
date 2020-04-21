1d6666bc8c85492cb3dfe110263dc63ebeca996e
Joe Redmon <joesredmon@gmail.com>
2020-04-21 12:26:33 -0700

    local css and html changes

--- test1.js
+++ test1.js
@@ -1,11 +1,1 @@
-// // ellipse (100, 100, 100, 100);
-
-// function greeting(){
-//     var hello = prompt("Hello!");
-// return document.write("Hello");
-// };
-
-function askName() {
-    var userName = prompt('What is your name?');
-    return document.write('<h3>' + 'Hello, ' + userName + '</h3>');
-};
+// var hello = prompt("Hello, how are you?");
